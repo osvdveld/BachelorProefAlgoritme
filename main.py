d430c148f277395a8d24fb46b5744459b7cf0e2f
Oscar Vandevelde <oscar.vandevelde@ugent.be>
2025-11-28 15:38:42 +0100

    What?

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def lees_studenten_voorkeuren(bestandsnaam):
             projecten = [project.strip() for project in delen[1:]]
 
             voorkeuren[naam] = projecten
-            print(projecten)
+        print(voorkeuren)
 
     return voorkeuren
 
